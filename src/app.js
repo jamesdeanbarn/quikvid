76b3e47ba566493ef4398426d7426482ee21104f
jamesdeanbarn <jamesdeanbarnett@gmail.com>
2020-10-20 13:21:55 +0100

    code cleanup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
         <h1>Hello</h1>
@@ -8,4 +8,3 @@ function App() {
   );
 }
 
-export default App;
